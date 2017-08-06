427c1d1629b15773acd73e4b36145732530f9e3b
Gregor Woiwode <gw@co-it.eu>
2017-08-06 22:09:06 +0200

    install clarity-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ClarityModule } from 'clarity-angular';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
