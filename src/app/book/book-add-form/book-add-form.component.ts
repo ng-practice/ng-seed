7cfe65210cc6c4e4b95a801a3b9f558f3e53343d
Gregor Woiwode <gw@co-it.eu>
2017-08-08 10:25:38 +0200

    add directive click.once

---
+++ src/app/book/book-add-form/book-add-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ws-book-add-form',
+  templateUrl: './book-add-form.component.html',
+  styleUrls: ['./book-add-form.component.scss']
+})
+export class BookAddFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -3,19 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { ClarityModule } from 'clarity-angular';
 
+import { UtilitiesModule } from '../utilities/utilities.module';
+
 import { BookGridComponent } from './book-grid/book-grid.component';
 import { BookGridElementComponent } from './book-grid-element/book-grid-element.component';
 import { ViewToggleComponent } from './view-toggle/view-toggle.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookListElementComponent } from './book-list-element/book-list-element.component';
+import { BookAddFormComponent } from './book-add-form/book-add-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
-    ClarityModule.forChild()
+    ClarityModule.forChild(),
+
+    UtilitiesModule
   ],
-  declarations: [BookGridElementComponent, BookGridComponent, ViewToggleComponent, BookListComponent, BookListElementComponent],
-  exports: [BookGridElementComponent, BookGridComponent, ViewToggleComponent, BookListComponent]
+  declarations: [BookGridElementComponent, BookGridComponent, ViewToggleComponent, BookListComponent, BookListElementComponent, BookAddFormComponent],
+  exports: [BookGridElementComponent, BookGridComponent, ViewToggleComponent, BookListComponent, BookAddFormComponent]
 })
 export class BookModule { }
